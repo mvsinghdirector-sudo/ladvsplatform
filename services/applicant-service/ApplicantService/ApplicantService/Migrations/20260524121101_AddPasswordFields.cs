6c037f7bc4a2239b4156b936d9e46b4ec87e95b0
Mohan Veer Singh <mvsingh.director@gmail.com>
2026-05-24 09:15:09 -0700

    fix: Add missing project files

---
+++ services/applicant-service/ApplicantService/ApplicantService/Migrations/20260524121101_AddPasswordFields.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicantService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Applicants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Applicants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Applicants");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Applicants");
+        }
+    }
+}
